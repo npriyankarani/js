b3b713c532176192236b0bac211080681d66c76a
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-01 06:24:12 +0000

    Add 01_basics/03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let x = 5;
+console.log(typeof x);
+console.log(typeof(x));
+
+
+let y="33"
+console.log(typeof y);
+console.log(typeof(y));
+let valueInNumber=Number(y);
+console.log(typeof valueInNumber);
+
+//"33"==>33
+//"33abc"==>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1==>true;0=>false
+//""=>false
+//"priyanka" =>true
+
+//********************************OPERATIONS ***************/
+let value=3
+let negValue=-value
+console.log(negValue);
+
+console.log(+true)  //1
+console.log(+"")    //0
