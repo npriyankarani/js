014156444761c3ceb2a27b1afd2926d05e6f372e
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-01 03:26:44 +0000

    Update

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//trat all js code as newer version
+
+//alert(3+3) // we are using nodejs,not browser
+
+const accountId=144553
+let accountEmail="p@123.com"
+
+//number =>2 to power 53
+//bigint
+//string ==>'' or "" or ``
+//boolean
+//null ==>stand alone value
+//undefined ==>value is not assigned
+//symbol=> unique used in react
+
+//object
+console.log(typeof accountId)
+console.log(typeof null)
+console.log(typeof undefined)
