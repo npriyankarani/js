36c27bd775de6e6d5973d68176b4a2cebe603cfc
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-01 03:11:49 +0000

    Update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+let accountState;
+
+accountEmail="abc@google.com"
+accountPassword="67890"
+accountCity="Noida"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*prefer not to use var
+because of its functional scope and block scope
+*/
