e4370adf4420d1bfed778e975ceff6559f7cd198
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-02 13:06:45 +0000

    Add block scope examples using let, const, and var

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+ let a=100
+    const b=20
+    var c=30
+if (true)
+{
+    let a=10
+    const b=20
+    var c=30
+    console.log("inner",a);
+}
+
+console.log("outer:",a);
+console.log(b);
+console.log(c);
+
+ 
