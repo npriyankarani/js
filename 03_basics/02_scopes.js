bff5275fcf0aa1f91c6126d9d4c41d96aba86ce2
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-02 13:48:19 +0000

    Add block scope examples using let, const, and var

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,4 +13,44 @@ console.log("outer:",a);
 console.log(b);
 console.log(c);
 
+function one(){
+    const username="priya"
+
+    function two()
+    {
+        const website="utube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()//not executed
+}
+one()
+
+if(true)
+{
+    const username="priya"
+    if(username==="priya")
+    {
+        const website="utube"
+        console.log(username + website);
+    }
+    //console.log(website)//error cant be accessed
+}
+//console.log(username)//error cant be accessed
+
+//*****INTERESTING*****
+console.log(addone(5))//6
+function addone(num)
+{
+    return num+1
+}
+
+addtwo(5)//error
+const addtwo=function(num2)
+{
+    return num2+1
+}
+
+
+
  
