3c26b6166d7e4bf29537b49e7d284c0398e8f65c
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-02 03:27:30 +0000

    Add 03_basics/01_functions.js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+    console.log("H")
+    console.log("I")
+    console.log("T")
+    console.log("E")
+    console.log("S")
+    console.log("h")
+
+}
+sayMyName()
+
+function addTwoNumbers(){
+    let a=10,b=5;
+    let sum;
+    sum=a+b;
+    console.log(sum)
+}
+addTwoNumbers()
+
+function loginUserMessage(username)
+{
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Priyanka"))
