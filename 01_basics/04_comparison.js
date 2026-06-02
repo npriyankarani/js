a59305d5b7f77ca3c546de03584cabdd28c67ae4
npriyankarani <npriyanka.rani2003@gmail.com>
2026-06-02 03:28:02 +0000

    Commit 01_basics/04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,5 +1,13 @@
 console.log("2">1); //true
-console.log(null>0)
+console.log(null>0)//false
+console.log(null==0) //false
+console.log(null<0) //false
+
+console.log(undefined>0) //false
+console.log(undefined==0) //false
+console.log(undefined<0) //false
+
+//null and undefined are only equal to themselves and not to any other value    
 console.log(null==0)
 console.log(null<0)
 console.log(undefined>0)
